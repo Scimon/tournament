257edd7bae0190d278ea113745af40725b4c9653
Simon Proctor <simon.proctor@photobox.com>
2013-11-06 10:23:14 +0000

    Ok, doing a tournament system

--- src/main/java/com/photobox/appclasses/Player.java
+++ src/main/java/com/photobox/appclasses/Player.java
@@ -1,5 +1,8 @@
 package com.photobox.appclasses;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
 /**
  * Created with IntelliJ IDEA.
  * User: simon
@@ -7,5 +10,32 @@ package com.photobox.appclasses;
  * Time: 10:01
  * A Player in a tournament
  */
+@XmlRootElement
 public class Player {
+    public String firstName;
+    public String lastName;
+    public Date dateOfBirth;
+
+    public Player() {
+    }
+
+    public Player(String name) {
+        this.firstName = name;
+    }
+
+    public Player(String name, Date date) {
+        this.firstName = name;
+        this.dateOfBirth = date;
+    }
+
+    public Player(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+
+    public Player(String first, String last, Date date) {
+        this.firstName = first;
+        this.lastName = last;
+        this.dateOfBirth = date;
+    }
 }
--- src/main/java/com/photobox/restapp/Players.java
+++ src/main/java/com/photobox/restapp/Players.java
@@ -1,15 +1,25 @@
 package com.photobox.restapp;
 
+import com.photobox.appclasses.Player;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
-@Path("myresource")
-public class MyResource {
+@Path("players")
+public class Players {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Player> getPlayers() {
+        List<Player> PlayerList = new ArrayList<Player>();
+        PlayerList.add( new Player( "Simon", "Proctor") );
+        PlayerList.add( new Player( "Claudio", "Ferrete") );
+        return PlayerList;
+    }
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -17,9 +27,11 @@ public class MyResource {
      *
      * @return String that will be returned as a text/plain response.
      */
+    /*
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Got it!";
     }
+    */
 }
